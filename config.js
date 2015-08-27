ff7ad0abed075df9d4de014d74609cfb2df36253
JerryGong <jerryygon@gmail.com>
2015-08-27 17:53:38 +0800

    ready for Angular way Branch

--- config.js
+++
@@ -1,7 +0,0 @@
-var Config = {
-	'dataBase':'mongodb://localhost/movie',
-	'port': process.env.PORT || 3000,
-	'secret':'jerrylovesanna4ever'
-
-};
-module.exports = Config;
--- test/demo.js
+++
@@ -1,5 +0,0 @@
-exports.sanitize = function(word){
-	// console.log('333');
-	// return word.toLowerCase().split('-').join(' ');
-	return word.toLowerCase().replace(/-/g,' ');
-};
